73c49c7c096085f7d6ba2d45879586ce06b23051
Daniel Ryan <d@dryan.com>
2018-06-25 18:23:29 -0400

    setting up grunt for compiling assets

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+let banner = '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n';
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    // Package
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Compass
+    compass: {
+      build: {
+        options: {
+          sassDir: 'assets/scss',
+          cssDir: 'assets/css',
+          outputStyle: 'expanded'
+        }
+      }
+    },
+
+    // Clean
+    clean: {
+      pre: ['styleguide', 'assets/css'],
+      post: ['.sass-cache']
+    },
+
+    // Watch
+    watch: {
+      sass: {
+        files: ['assets/scss/**/*.{sass,scss}'],
+        tasks: ['compass', 'postcss'],
+        options: {
+          livereload: true
+        }
+      },
+      javascript: {
+        files: ['assets/js/**/*.js', 'assets/js/**/*.jsx', '!assets/js/**/*.min.js'],
+        tasks: ['jshint', 'browserify:dist', 'uglify'],
+        options: {
+          livereload: true
+        }
+      },
+      configFiles: {
+        files: ['Gruntfile.js'],
+        options: {
+          reload: true
+        }
+      }
+    },
+
+    // PostCSS (autoprefixer, etc.)
+    postcss: {
+      options: {
+        map: {
+          inline: false,
+          annotation: 'assets/css/maps/'
+        },
+
+        processors: [require('pixrem')(), require('autoprefixer')({ browsers: 'last 2 versions' }), require('cssnano')({ zindex: false })]
+      },
+      dist: {
+        src: 'assets/css/*.css'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: banner,
+        compress: true,
+        mangle: true,
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          // Example: 'assets/js/core.min.js': ['assets/js/core.js'],
+        }
+      }
+    },
+
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      target: {
+        src: ['assets/js/*.jsx', 'assets/js/*.js', '!assets/js/*.min.js', '!assets/js/vendors/*']
+      }
+    },
+
+    browserify: {
+      dist: {
+        files: {
+          // Example 'assets/js/core.js': 'assets/js/core.jsx',
+        },
+        options: {
+          transform: [['babelify', { presets: ['es2015'] }]],
+          browserifyOptions: {
+            debug: true
+          }
+        }
+      }
+    }
+  });
+
+  // Load NPM Tasks
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Register Grunt tasks
+  grunt.registerTask('default', ['clean:pre', 'compass', 'postcss', 'jshint', 'browserify:dist', 'uglify', 'clean:post']);
+};
--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # marchon-data-viz
+
 Visualization of March On's Operation Marching Orders data
 
 Here is a link to a preview for a current version of March On Comment webpage.
 https://ragtagopen.github.io/marchon-data-viz/
 
-## Compiling Styles
-Forthcoming
+## Installing Dependencies
 
-### Installing Sass
-Forthcoming
+1.  Install Node/NPM: If you're on a Mac running [Homebrew](https://brew.sh), run `brew install node` on the command line. Otherwise, [follow these instructions](https://www.npmjs.com/get-npm).
+2.  Install Sass by running `npm -g install sass` on the command line.
+3.  Install everything else by running `npm install` on the command line.
 
-### Compiling Assets
-Forthcoming
+## Compiling Assets with Grunt
+
+In the root directory of the project run `grunt watch` on the command line. Any edits made to "assets/scss/app.scss" or one of the files in "assets/scss/base" will recompile "app.css" for you.
+
+The configuration also supports Live Reload. Install one of [the browser extensions](http://livereload.com/extensions/) to have the page automatically refresh whenever an asset is recompiled.
